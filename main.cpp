27c0ef147cbded47d379fcd0c598f4a90178f613
akashdiphazra <princehazra.96@gmail.com>
2022-08-30 19:47:11 +0530

    new files changed

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
+#include <algorithm>
 #include <functional>
 #include <iostream>
+#include <vector>
 
 int main() {
   [out = std::ref(std::cout << "Hello")]() { out.get() << " World"; }();
+  std::cout << std::endl;
+  uint_fast64_t n = 1, m = 8, p = 8;
+  std::vector<std::vector<std::vector<uint_fast64_t>>> vvv(
+      n, std::vector<std::vector<uint_fast64_t>>(
+             m, std::vector<uint_fast64_t>(p, 200)));
+  for (uint_fast64_t i{}; i < n; i++) {
+    for (uint_fast64_t j{}; j < m; j++) {
+      for (uint_fast64_t k{}; k < p; k++) {
+        std::cout << vvv[i][j][k] << " ";
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  }
   return 0;
 }
