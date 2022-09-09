5a8c792800a0825594c6e83ff2626b28269d7059
akashdiphazra <princehazra.96@gmail.com>
2022-09-09 21:29:51 +0530

    project structure changed with standard software development setup

---
+++ build/CMakeFiles/akash.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/akash.dir/src/main.cpp.o"
+  "CMakeFiles/akash.dir/src/main.cpp.o.d"
+  "akash"
+  "akash.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/akash.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
